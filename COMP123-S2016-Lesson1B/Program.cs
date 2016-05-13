3d5ec7b84aeb7e98ae1f6c4d483ac43a345e3051
Harry Dai <dz655618@live.com>
2016-05-13 10:45:35 -0400

    Initial Commit

--- COMP123-S2016-Lesson1B/Program.cs
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Harry Dai
+ * Date: 2016/05/13
+ * Student ID: 300839401
+ * 
+ * Descriptions: THis program demonstrates GitHub and version control best pratices.
+ * 
+ * Version: 0.1
+ */
+
 namespace COMP123_S2016_Lesson1B
 {
     class Program
